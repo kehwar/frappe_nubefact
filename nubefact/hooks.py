86bf9b9b6cf95002b966bbdfd6ce8c620862c67e
kehwar <erickkwr@gmail.com>
2026-02-28 04:51:54 +0000

    refactor(hooks): update ignored links on delete to include Nubefact API Log

--- nubefact/hooks.py
+++ nubefact/hooks.py
@@ -202,7 +202,7 @@ scheduler_events = {
 # Ignore links to specified DocTypes when deleting documents
 # -----------------------------------------------------------
 
-# ignore_links_on_delete = ["Communication", "ToDo"]
+ignore_links_on_delete = ["Nubefact API Log"]
 
 # Request Events
 # ----------------
